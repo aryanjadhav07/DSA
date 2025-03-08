55e1289d0842706e680507a20e10132e54dd7dfc
MIBCS <aryanjadhavart2005@gmail.com>
2025-03-08 20:27:30 +0530

    Done with the 3rd patterns

--- patterns.cpp
+++ patterns.cpp
@@ -23,6 +23,17 @@ void pattern2(int n){
 
 }
 
+void pattern3(int n){
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j <<  " " ;
+        }
+        cout << endl;
+    }
+
+}
+
 int main(){
     int n;
     cout << "Enter the number of rows:";
