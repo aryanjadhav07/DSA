1b3a69e0383cb50ec7fc866dae0c9edbbe4857b3
MIBCS <aryanjadhavart2005@gmail.com>
2025-03-08 20:19:54 +0530

    Done with the 2nd patterns

--- patterns.cpp
+++ patterns.cpp
@@ -12,10 +12,21 @@ void pattern1(int n){
 
 }
 
+void pattern2(int n){
+    
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<i; j++){
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+
+}
+
 int main(){
     int n;
     cout << "Enter the number of rows:";
     cin >> n;
-    pattern1(n);
+    pattern2(n);
     return 0;
 }
