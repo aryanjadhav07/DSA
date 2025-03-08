f646cb0c6a6c1b8995ad8f6d8a4fab7891ff75b5
MIBCS <aryanjadhavart2005@gmail.com>
2025-03-08 20:45:29 +0530

    Done with the 4th and 5th patterns

--- patterns.cpp
+++ patterns.cpp
@@ -34,10 +34,32 @@ void pattern3(int n){
 
 }
 
+void pattern4(int n){
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << i <<  " " ;
+        }
+        cout << endl;
+    }
+
+}
+
+void pattern5(int n){
+    
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<n-i+1; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+}
+
 int main(){
     int n;
     cout << "Enter the number of rows:";
     cin >> n;
-    pattern2(n);
+    pattern5(n);
     return 0;
 }
